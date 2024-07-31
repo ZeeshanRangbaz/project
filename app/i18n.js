cefb19bf680fe8f0324b65a123d2c8750bf53db5
ZeeshanRangbaz <zeeshanshani102030@gmail.com>
2024-07-31 16:21:03 +0500

    "added comment"

--- app/i18n.js
+++ app/i18n.js
@@ -1,29 +1,27 @@
-"use client"
+"use client";
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import i18n from "i18next";
-import {initReactI18next } from "react-i18next";
+import { initReactI18next } from "react-i18next";
 import eng_lang from "../locales/eng/eng_lang.json";
 import urdu_lang from "../locales/urdu/urdu_lang.json"; // Adjust path as needed
 
 const resources = {
-    en: {
-        translation: eng_lang
-    },
-    ur: {
-        translation: urdu_lang
-    }
+  en: {
+    translation: eng_lang,
+  },
+  ur: {
+    translation: urdu_lang,
+  },
 };
 
-i18n.use(initReactI18next)
-    .init({
-        resources,
-        lng: "en", 
-        keySeparator: ".",
-        interpolation: {
-            escapeValue: false
-        }
-    });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  keySeparator: ".",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
-
-export default i18n
+export default i18n;
